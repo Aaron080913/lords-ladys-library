8b7a4f3752cd0dd3e5942ff692254235dd7fdee8
Aaron Jones <aaronjjj95@aol.com>
2024-09-04 10:44:52 +0000

    initial commit

---
+++ lords_ladys_library/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for lords_ladys_library project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lords_ladys_library.settings')
+
+application = get_asgi_application()
